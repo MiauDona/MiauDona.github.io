1689a2a23e89951f31aebe1458cb2cb17c6f27a0
MiauDona <donanferyt@gmail.com>
2023-11-09 22:52:23 +0100

    Cambio de apariencia y mejora en paleta de colores

---
+++ js/index.js
@@ -0,0 +1,1 @@
+var prevScrollpos = window.scrollY;window.onscroll = function() {var currentScrollPos = window.scrollY;if (prevScrollpos > currentScrollPos) {document.querySelector(".menu").style.top = "0";} else {document.querySelector(".menu").style.top = "-110px";}prevScrollpos = currentScrollPos;};
